bf29efecb36f43e1ea0d6b17e7e2c35d521d7f5d
Chris Ford <christophertford@gmail.com>
2014-04-02 20:39:08 +0100

    Detect constants.

--- src/testgen/core.clj
+++ src/testgen/core.clj
@@ -5,5 +5,13 @@
      	(list 'is (list '= (list fnname arg) (eval (list fnname arg))))
 	(catch Exception e (list 'is (list 'thrown? (.getClass e) (list fnname arg))))))
 
-
 (defn testgen [fndef] nil)
+
+(defn is-constant? [x] (not (symbol? x)))
+
+(defn constants [form]
+  (filter is-constant? (flatten form)))
+
+(comment
+  (constants '(fn is-four [x] (= 88 [44])))
+  )
