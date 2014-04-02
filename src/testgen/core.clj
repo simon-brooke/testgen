06f16d561fa3b39c64d0409d18a1e5fce7aa4d7c
Simon Brooke <simon@journeyman.cc>
2014-04-02 20:10:27 +0100

    First function written

--- src/testgen/core.clj
+++ src/testgen/core.clj
@@ -4,3 +4,8 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn write-test [fnname arg] 
+     (list 'is (list '= (list fnname arg) (eval (list fnname arg)))))
+
+(defn testgen [fndef] nil)
