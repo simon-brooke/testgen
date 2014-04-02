f65185b963dbda9f026cb5c1578c9e5d05f4c894
Simon Brooke <simon@journeyman.cc>
2014-04-02 20:51:11 +0100

    Now handles things which throw exceptions

--- src/testgen/core.clj
+++ src/testgen/core.clj
@@ -1,11 +1,9 @@
 (ns testgen.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
 (defn write-test [fnname arg] 
-     (list 'is (list '= (list fnname arg) (eval (list fnname arg)))))
+     (try
+     	(list 'is (list '= (list fnname arg) (eval (list fnname arg))))
+	(catch Exception e (list 'is (list 'thrown? (.getClass e) (list fnname arg))))))
+
 
 (defn testgen [fndef] nil)
